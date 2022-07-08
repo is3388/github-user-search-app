dfab3d01f4a774648be89464699d6db9050078dc
is3388 <meiychin@yahoo.com>
2022-07-08 12:55:08 -0400

    Added Alert component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
+import Alert from './components/layout/Alert'
 import Home from './pages/Home'
 import About from './pages/About'
 import NotFound from './pages/NotFound'
@@ -14,9 +15,9 @@ function App() {
     <Router>
     <div className='flex flex-col justify-between h-screen'>
       <Navbar />
-      <main className='container mx-auto px-3 pb-12'>
+      <main className='container mx-auto px-3 pb-12'>        
         <Routes>
-          <Route path='/' exact element={<Home />} />
+          <Route path='/' exact element={<><Alert /><Home /></>} />
           <Route path='/about' element={<About />} />
           <Route path='/notfound' element={<NotFound />} />
           <Route path='/*' element={<NotFound />} />
