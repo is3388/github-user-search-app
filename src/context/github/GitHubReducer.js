278455f8824c47d0aeb7240d1e6ac6b51c17dbcd
is3388 <meiychin@yahoo.com>
2022-07-11 10:17:32 -0400

    Added additional user info such as location, website, followers, twitter, public repos, twitter and ghist to User profile page

--- src/context/github/GitHubReducer.js
+++ src/context/github/GitHubReducer.js
@@ -11,6 +11,11 @@ const githubReducer = (state, action) => {
             user: action.payload,
             loading: false
         }
+        case 'GET_REPOS':
+            return {...state,
+            repos: action.payload,
+            loading: false
+        }
         case 'SET_LOADING':
             return {...state,
             loading: true
